0f4b855483287d8ff2cae88ee43eef814adaf479
juni Park <juni5184@naver.com>
2019-12-06 11:39:05 +0900

    mouth detect only

--- mouth_detection2.py
+++ mouth_detection2.py
@@ -1,86 +1,58 @@
-# USAGE
-# python mouth_detection.py -p "shape_predictor_68_face_landmarks.dat" -i "images/example_01.jpg"
-
-# import the necessary packages
-from imutils import face_utils
 import numpy as np
-import argparse
-import imutils
-import dlib
 import cv2
-from imutils.face_utils import FaceAligner
-from imutils.face_utils import rect_to_bb
-from matplotlib import pyplot as plt
+import dlib
+from imutils import face_utils
+import imutils
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True,
-	help="path to facial landmark predictor")
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
-args = vars(ap.parse_args())
+imagePath = "ismouthopen/KakaoTalk_20191205_224232042.jpg"
+cascPath = "haarcascade_frontalface_default.xml"
 
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor and the face aligner
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
-fa = FaceAligner(predictor, desiredFaceWidth=256)
+PREDICTOR_PATH = "shape_predictor_68_face_landmarks.dat"
 
-# load the input image, resize it, and convert it to grayscale
-image = cv2.imread(args["image"])
-image = imutils.resize(image, width=800)
+JAWLINE_POINTS = list(range(0, 17))
+RIGHT_EYEBROW_POINTS = list(range(17, 22))
+LEFT_EYEBROW_POINTS = list(range(22, 27))
+NOSE_POINTS = list(range(27, 36))
+RIGHT_EYE_POINTS = list(range(36, 42))
+LEFT_EYE_POINTS = list(range(42, 48))
+MOUTH_OUTLINE_POINTS = list(range(48, 61))
+MOUTH_INNER_POINTS = list(range(61, 68))
+
+# Create the haar cascade  
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+predictor = dlib.shape_predictor(PREDICTOR_PATH)
+
+# Read the image  
+image = cv2.imread(imagePath)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# show the original input image and detect faces in the grayscale image
-# cv2.imshow("Input", image)
-rects = detector(gray, 2)
+# Detect faces in the image  
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.05,
+    minNeighbors=5,
+    minSize=(100, 100),
+    flags=cv2.CASCADE_SCALE_IMAGE
+)
 
-# loop over the face detections
-for rect in rects:
-	# extract the ROI of the *original* face, then align the face
-	# using facial landmarks
-	(x, y, w, h) = rect_to_bb(rect)
-	# faceOrig = imutils.resize(image[y:y + h, x:x + w], width=256)
-	faceAligned = fa.align(image, gray, rect)
+print("Found {0} faces!".format(len(faces)))
 
-	# display the output images
-	# cv2.imshow("Original", faceOrig)
-	# cv2.imshow("Aligned", faceAligned)
-	# cv2.waitKey(0)
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    #cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
+    # Converting the OpenCV rectangle coordinates to Dlib rectangle
+    dlib_rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
 
-# load the input image, resize it, and convert it to grayscale
-image = imutils.resize(faceAligned, width=256)
-gray = cv2.cvtColor(faceAligned, cv2.COLOR_BGR2GRAY)
+    landmarks = np.matrix([[p.x, p.y]
+                           for p in predictor(image, dlib_rect).parts()])
 
-rects = detector(gray, 1)
+    landmarks_display = landmarks[MOUTH_INNER_POINTS + MOUTH_OUTLINE_POINTS]
 
-# ===========================image align===============================
-# loop over the face detections
-# detect face
-roi = rects[0] # region of interest
-shape = predictor(gray, roi)
-shape = face_utils.shape_to_np(shape)
+    for idx, point in enumerate(landmarks_display):
+        pos = (point[0, 0], point[0, 1])
+        cv2.circle(image, pos, 2, color=(0, 255, 255), thickness=-1)
 
-# extract mouth
-mouth = shape[48:68]
-top = min(mouth[:,1])
-bottom = max(mouth[:,1])
-
-# extend contour for masking
-mouth = np.append(mouth, [ w-1, mouth[-1][1] ]).reshape(-1, 2)
-mouth = np.append(mouth, [ w-1, h-1 ]).reshape(-1, 2)
-mouth = np.append(mouth, [ 0, h-1 ]).reshape(-1, 2)
-mouth = np.append(mouth, [ 0, mouth[0][1] ]).reshape(-1, 2)
-contours = [ mouth ]
-
-# generate mask
-mask = np.ones((h,w,1), np.uint8(0)) * 255 # times 255 to make mask 'showable'
-cv2.drawContours(mask, contours, -1, 0, -1) # remove below mouth
-
-# apply to image
-result = cv2.bitwise_and(image, image, mask = mask)
-result = result[top:bottom, roi.left():roi.left()+roi.width()] # crop ROI
-#cv2.imwrite('result.png', result);
-cv2.imshow('masked image', result)
-cv2.waitKey(0)
+cv2.imshow("Landmarks found", image)
+cv2.waitKey(0) 
